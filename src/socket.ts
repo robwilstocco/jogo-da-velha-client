61eaab2302602881c181aa44b37557490c46ba16
Robert Wilson <robwilstocco@gmail.com>
2025-04-17 12:05:07 -0300

    io URL changed

--- src/socket.ts
+++ src/socket.ts
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3000");
+export const socket = io("https://jogo-da-velha-server.onrender.com"); //"http://localhost:3000";
